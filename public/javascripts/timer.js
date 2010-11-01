35b012eeaccea5be158d4f9e0b1e8946fdc13bc3
Mathieu Berube <mathieu.berube@gmail.com>
2010-11-01 00:59:54 -0400

    pomodoro countdown

--- public/javascripts/timer.js
+++ public/javascripts/timer.js
@@ -1,5 +1,54 @@
-// Override the onload event
 window.onload = function()
 {
-    $("#timer").text("25:00:00")
+    var timeInterval = 25*60*1000
+    var endTime = new Date(new Date().getTime() + timeInterval)
+    tick(endTime)
+}
+
+/**
+ *
+ * @param endTime Date object at which the timer will end
+ */
+function tick(endTime)
+{
+    var now = new Date();
+    var interval = endTime.getTime() - now.getTime();
+    var timeToDisplay = formatTime(interval)
+    $('#timer').html(timeToDisplay)
+
+    if(interval > 0) {
+        var nextTick = function() {
+            tick(endTime)
+        }
+        setTimeout(nextTick, 100)
+    } else {
+        // TODO play sound
+    }
+}
+
+/**
+ * @param interval interval to display in milliseconds
+ */
+function formatTime(interval) {
+    var remainingTimeInSeconds = parseInt(interval/1000);
+    var s = parseInt(remainingTimeInSeconds%60)
+    remainingTimeInSeconds = parseInt(remainingTimeInSeconds / 60)
+    var m = parseInt(remainingTimeInSeconds%60)
+    remainingTimeInSeconds = parseInt(remainingTimeInSeconds / 60);
+    var h = parseInt(remainingTimeInSeconds)
+
+    // add zero in front of number < 10
+    var timeToDisplay = fillWithZero(m)+":" + fillWithZero(s);
+    if(h > 0) {
+        timeToDisplay = h+":" + timeToDisplay
+    }
+    return timeToDisplay
+}
+
+function fillWithZero(i)
+{
+    if(i<10) {
+        i = "0" + i;
+    }
+    return i;
 }
