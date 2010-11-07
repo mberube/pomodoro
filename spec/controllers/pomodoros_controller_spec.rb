b3e902a31bff97cf2af8b14f8af626fa054ff7d4
Mathieu Berube <mathieu.berube@gmail.com>
2010-11-07 08:22:28 -0500

    add simple fixtures for specs

--- spec/controllers/pomodoros_controller_spec.rb
+++ spec/controllers/pomodoros_controller_spec.rb
@@ -1,14 +1,21 @@
 require 'spec_helper'
 
 describe PomodorosController do
-  it "new should succeed" do
+  fixtures :users
+  it "new should succeed without login" do
     get :new
     response.should be_success
   end
 
-  it "index should redirect to sessions" do
+  it "index should redirect to sessions when no user is logged in" do
     get :index
     response.should redirect_to(sessions_path)
     flash[:error].should include("You must be logged in")
+  end
+
+  it "index should be available when user is logged in" do
+    login_as_test_user
+    get :index
+    response.should be_success
   end
 end
