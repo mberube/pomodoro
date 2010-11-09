a6791f785c05aa998ac9e05646e8c2098977bc16
Mathieu Berube <mathieu.berube@gmail.com>
2010-11-08 23:25:12 -0500

    rename to PomodoroPlanner

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Pomodoro::Application.config.secret_token = '01751d93929168cbcc4ea9b8002045a3fba97b6e6c3ac6a2f5c37acd9ae203912eaec711ac3007bf8ae16159052f3a4d3b00bc7b90cb5f4b6d9f24bbc9df8245'
+PomodoroPlanner::Application.config.secret_token = '01751d93929168cbcc4ea9b8002045a3fba97b6e6c3ac6a2f5c37acd9ae203912eaec711ac3007bf8ae16159052f3a4d3b00bc7b90cb5f4b6d9f24bbc9df8245'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Pomodoro::Application.routes.draw do
+PomodoroPlanner::Application.routes.draw do
 	match '/auth/:provider/callback', :to => 'sessions#create'
   # The priority is based upon order of creation:
   # first created -> highest priority.
