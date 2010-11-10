eaa10224f95d50b0918c57f5dd2b9069c8ca8b9c
Mathieu Berube <mathieu.berube@gmail.com>
2010-11-09 23:27:57 -0500

    go to pomodoro page if user is aleady logged in

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe SessionsController do
+  it "should show home if user is not logged in" do
+    get :index
+    response.should be_success
+  end
+
+  it "should redirect to pomodoros if user is logged in" do
+    login_as_test_user
+    get :index
+    response.should redirect_to(pomodoros_path)
+  end
+end
