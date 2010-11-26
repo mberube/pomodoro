8eba236b8c61686d639286429f448303092b2896
Mathieu Berube <mberube@pyxis-tech.com>
2010-11-26 01:07:15 -0500

    make sure that pomodoros/new can always be rendered even when user is not logged in

--- spec/controllers/pomodoros_controller_spec.rb
+++ spec/controllers/pomodoros_controller_spec.rb
@@ -2,10 +2,14 @@ require 'spec_helper'
 
 describe PomodorosController do
   fixtures :users
-  it "new should succeed without login" do
-    get :new
-    response.should be_success
+  describe "pomodoros with rendered view" do
+    render_views
+    it "new should succeed without login" do
+      get :new
+      response.should be_success
+    end
   end
+
 
   it "index should redirect to sessions when no user is logged in" do
     get :index
